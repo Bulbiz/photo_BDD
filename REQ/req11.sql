7988b0c2ea001724b8242f53f0ca941ae67d7939
PHOL ASA Remy <remy.phol@etu.univ-paris-diderot.fr>
2021-05-08 08:32:35 +0200

    city -> departement

--- REQ/req11.sql
+++ REQ/req11.sql
@@ -1,9 +1,9 @@
--- Requête 11 : "Récupère les départements contenant plusieurs adresses différentes".
+-- Requête 11 : "Récupère les villes contenant plusieurs adresses différentes".
 -- -> Auto jointure + GROUP BY
 
-SELECT a1.departement
+SELECT a1.city
 FROM address a1,
      address a2
 WHERE a1.aid <> a2.aid
-  AND a1.departement = a2.departement
-GROUP BY a1.departement;
+  AND a1.city = a2.city
+GROUP BY a1.city;
