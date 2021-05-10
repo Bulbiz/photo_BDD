c3ba65e3f080b657720e5f08ce7d2c65f658b151
EmileRolley <erolley@tutamail.com>
2021-05-10 13:50:15 +0200

    feat(req): modifies the first request in a more complicated one

--- REQ/req1.sql
+++ REQ/req1.sql
@@ -1,11 +1,50 @@
--- Requête 1 : "Récupère tous les commentaires concernant les photographies du photographe numéro 2"
+-- Requête 1 : "Récupère le prix d'achat de tous les éléments d'une commande."
 
-SELECT photography.title,
-       review_date,
-       rate,
-       remark
-FROM photographer
-NATURAL JOIN photography
-NATURAL JOIN photographycopy
-NATURAL JOIN review
-WHERE photographer_id = 2;
+CREATE OR REPLACE FUNCTION get_puchase_price_of_cmd(INT)
+RETURNS TABLE(elem_id INT, print_price INT, digital_price INT, copy_id INT, change_date TIMESTAMP)
+AS $func$
+  DECLARE
+    elem_id INT;
+    rec RECORD;
+  BEGIN
+    FOR elem_id IN
+      -- Récupère tous les éléments de la commande.
+      SELECT DISTINCT s.elem_id
+      FROM shoppingcartelem s
+      WHERE s.cmd_id = $1
+        AND s.cmd_id IS NOT NULL
+    LOOP
+      RETURN QUERY EXECUTE
+        -- Pour chaque éléments,
+        -- récupère le dernier changement de prix avant la date de commande.
+        'SELECT DISTINCT s.elem_id,
+                  h.print_price,
+                  h.digital_price,
+                  s.copy_id,
+                  h.change_date
+        FROM command c
+        JOIN shoppingcartelem s ON (c.cmd_id = '|| $1 || '
+                                    AND s.cmd_id = c.cmd_id
+                                    AND s.cmd_id IS NOT NULL)
+        JOIN photographycopy pc ON pc.copy_id = s.copy_id
+        JOIN pricehistory h ON h.pid = pc.pid
+        WHERE c.command_date > h.change_date
+          AND s.elem_id = ' || elem_id || '
+        ORDER BY h.change_date DESC
+        LIMIT 1';
+    END LOOP;
+  END
+$func$ LANGUAGE PLPGSQL;
+
+-- Pour chaque éléments de la commande affiche son prix d'achat en fonction du type
+-- de la copie.
+SELECT res.elem_id,
+       CASE
+            (SELECT photo_type
+             FROM photographycopy
+             WHERE copy_id = res.copy_id)
+           WHEN 1 THEN res.print_price
+           ELSE res.digital_price
+       END AS purchase_price
+FROM get_puchase_price_of_cmd(7) res -- Récupère les prix d'achats des éléments de la commande 7.
+ORDER BY res.elem_id;
